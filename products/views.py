cc01829784719efc1046f53458b274f6d45dd1e1
Sorin05 <sorinandrei230587@gmail.com>
2022-11-05 14:17:15 +0000

    Added adjust  bag and remove from bag views, and cal subtotal

--- products/views.py
+++ products/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Product, Category
 from django.contrib import messages
 from django.db.models import Q
-from  django.db.models.functions import Lower
+from django.db.models.functions import Lower
+from .models import Product, Category
+
+# Create your views here.
 
 
 def all_products(request):
-    """ A view to show all products """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     query = None
@@ -29,7 +31,6 @@ def all_products(request):
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
-    if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -50,14 +51,14 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'urrent_sorting': current_sorting,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    """ A view to show an individual product """
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
