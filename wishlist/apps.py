7ca740e8a53a0dbaa358b799ebc7424663952f36
Sorin05 <sorinandrei230587@gmail.com>
2023-01-02 13:45:42 +0000

    Added wishlist, models ,views, urls and templates

---
+++ wishlist/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class WishlistConfig(AppConfig):
+    """
+    Configure wishlist application
+    """
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wishlist'
