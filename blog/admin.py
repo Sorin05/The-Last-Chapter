e0e9be3bdba1fe2feec187c7adf770f6c57ee9c8
Sorin05 <sorinandrei230587@gmail.com>
2023-01-03 15:31:33 +0000

    Configure blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+# Internal:
+from .models import BlogPost, Comment
+# -----------------------------------------------------------------------------
+
+
+class BlogAdmin(admin.ModelAdmin):
+    """
+    Class defining the BlogPost model in admin page
+    """
+    list_display = (
+        'id', 'owner', 'title', 'created_at', 'image', 'second_image',
+        'third_image', 'fourth_image', 'fifth_image',
+    )
+    ordering = ('owner', 'created_at')
+    search_fields = ('title', )
+    list_filter = ('owner', )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Comment model.
+    """
+    list_display = (
+        'posted_by', 'blog_article', 'posted_at'
+    )
+    ordering = ('posted_by', 'posted_at')
+    search_fields = ('posted_by', 'blog_article')
+    list_filter = ('posted_by', )
+
+
+admin.site.register(BlogPost, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
