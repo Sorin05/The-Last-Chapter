b1a0c1319339aa0ac93b5cdc531d3cf399bae2ed
Sorin05 <sorinandrei230587@gmail.com>
2023-01-05 17:27:42 +0000

    Fix checkoutsuccess bviews

--- checkout/views.py
+++ checkout/views.py
@@ -142,6 +142,7 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
@@ -163,16 +164,16 @@ def checkout_success(request, order_number):
             if user_profile_form.is_valid():
                 user_profile_form.save()
 
-        messages.success(request, f'Order successfully processed! \
-            Your order number is {order_number}. A confirmation \
-            email will be sent to {order.email}.')
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
-        if 'bag' in request.session:
-            del request.session['bag']
+    if 'bag' in request.session:
+        del request.session['bag']
 
-        template = 'checkout/checkout_success.html'
-        context = {
-            'order': order,
-        }
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
 
-        return render(request, template, context)
+    return render(request, template, context)
