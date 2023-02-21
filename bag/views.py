9f76168c096527f8b8c14a4402cb433426a91159
Sorin05 <sorinandrei230587@gmail.com>
2023-02-21 11:00:29 +0000

    Add 404 error page

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+    redirect,
+    reverse,
+    HttpResponse,
+)
+from django.contrib import messages
+from products.models import Product
+
 from django.contrib import messages
 
 from products.models import Product
