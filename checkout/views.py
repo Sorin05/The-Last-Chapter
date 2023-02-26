c6edc8539bfeaa5eb1e65a43ffe87da75de1a9cc
Sorin05 <sorinandrei230587@gmail.com>
2023-02-26 15:05:06 +0000

    Add environ

--- checkout/views.py
+++ checkout/views.py
@@ -72,7 +72,8 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, quantity in (
+                                item_data['items_by_size'].items()):
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -81,9 +82,8 @@ def checkout(request):
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
-                    messages.error(request,
-                                (
-                        "One of the products in your bag wasn't found in our database. "
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found"
                         "Please call us for assistance!")
                     )
                     order.delete()
